8c76c023da72e403009b4c6ee912248216c1d387
Inpassor <inpassor@mail.ru>
2015-11-27 09:48:04 +0300

    Update and rename .gitignore to components/EAction.php

---
+++ components/EAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * EAction class file.
+ * @version 0.0.1 (2015.09.17)
+ *
+ * @author Inpassor <inpassor@gmail.com>
+ * @link https://github.com/Inpassor
+ */
+
+class EAction extends CAction
+{
+	public $menu=array();
+}
+
+?>
